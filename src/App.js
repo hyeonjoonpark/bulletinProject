a9f9e61a689a0c9a36a09b353566163db9a34e13
“hyeonjoonpark” <jpjjoon1379@gmail.com>
2023-03-02 23:00:32 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+/* eslint-disable */
 import './App.css';
+import React, { Component } from 'react';
+import logo from "./images/logo.png";
+import Login from "./component/Login.js";
+import {Routes, BrowserRouter,  Route, Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logoWrap">
+          <img src={logo} className="logoImg" />
+      </div>
+
+      <div className="titleWrap">
+        <a>The Best board of today</a>
+      </div>
+
+      <div className="boardWrap">
+        Hello Dis is a BoardWrap.
+      </div>
+
+      <div className='navWrap'>
+        <BrowserRouter>
+          <button className='button'><Link to="/login">로그인</Link></button>
+
+          <button className='button'><a>마이페이지</a></button>
+
+          <button className='button'><a>게시물작성</a></button>
+
+          <button className='button'><a>채팅</a></button>
+
+          <button className='button'><a>문의하기</a></button>
+
+            <Routes>
+              <Route path='/login' element={ <Login /> } />
+            </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/component/Login.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return(
+        <div className="Login">
+            
+        </div>
+    )
+}
+
+export default Login;

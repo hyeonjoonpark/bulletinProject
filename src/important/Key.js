4d7234556080c3f64ce76087ff596fd8f032d784
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2023-03-11 16:45:34 +0900

    Delete src/important directory

--- src/important/Key.js
+++
@@ -1,3 +0,0 @@
-export const REST_API_KEY = 'f2d41640e1d56633ba751f4e370cee1e';
-export const REDIRECT_URI = 'http://localhost:3000/kakaologin';
-export const CLIENT_SECRET = 'FofN8b1MGUfmj118FaPtypoj0GH24Wmo';
